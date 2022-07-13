9e1c0bcbf01317bad303c3cdd9d094d8225dee17
ItzSamirr <plumino3124@gmail.com>
2022-07-13 14:44:28 +0300

    dumb

--- src/main/java/it/itzsamirr/samirlib/utils/Color.java
+++ src/main/java/it/itzsamirr/samirlib/utils/Color.java
@@ -9,13 +9,15 @@ import java.util.List;
 * @author ItzSamirr
 * Created at 07.06.2022
 **/
-@UtilityClass
-public class Color {
-    public String translate(String s){
+public final class Color {
+    private Color(){
+        throw new UnsupportedOperationException();
+    }
+    public static String translate(String s){
         return ChatColor.translateAlternateColorCodes('&', s);
     }
 
-    public List<String> translate(List<String> list){
+    public static List<String> translate(List<String> list){
         list.replaceAll(Color::translate);
         return list;
     }
